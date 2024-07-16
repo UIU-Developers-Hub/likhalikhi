6baca9d0893a10707b40d6f86987d2415e498235
Asadullah-Imran <asadullahimran19@gmail.com>
2024-07-16 23:53:28 +0600

    folder structur build up

---
+++ api/controllers/auth.js
@@ -0,0 +1,9 @@
+export const register=(req,res)=>{
+    res.send('THis is Register')
+}
+export const login=(req,res)=>{
+    res.send('THis is login')
+}
+export const logout=(req,res)=>{
+    res.send('THis is logout')
+}
--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import authRouter from './routes/authRoutes.js';
 
 const app = express();
 const PORT= 8080;
+
+//routing
+app.use('/api/auth',authRouter)
 
 
 app.listen(PORT,()=>{
