eb1bf38cf0d99feae6d18f4adf63406244245f55
Asadullah-Imran <asadullahimran19@gmail.com>
2024-07-18 01:20:21 +0600

    database connection done !

--- api/src/constants.js
+++ api/src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "likhalikhi";
+export const PORT = 8080;
--- api/src/index.js
+++ api/src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
+import app from "./app.js";
+import { PORT } from "./constants.js";
 import connectDB from "./db/db.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(PORT || 8000, () => {
+      console.log(`Server is runnign at port: ${PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed in index.js , error: ", err);
+  });
