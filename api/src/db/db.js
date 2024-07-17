0e6f443ddcd51295c0817a54a2cd3f0841cca1b4
Asadullah-Imran <asadullahimran19@gmail.com>
2024-07-18 01:08:34 +0600

    env setup done

---
+++ api/src/db/db.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+// console.log(DB_NAME)
+// console.log("salam->", process.env.MONGODB_URI);
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(`\n connectionInstace : ${connectionInstance}`);
+    console.log(
+      `\n MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (err) {
+    console.log("MONGODB connection in db.js error: ", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
