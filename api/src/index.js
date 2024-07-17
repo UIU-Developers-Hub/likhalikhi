62bae142d4bef3a5adffde75c48eb7a10a7d561a
Asadullah-Imran <asadullahimran19@gmail.com>
2024-07-17 21:32:51 +0600

    project structure update and prettier setup

---
+++ api/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import authRouter from "./routes/authRoutes.js";
+
+const app = express();
+const PORT = 8080;
+
+//routing
+app.use("/api/auth", authRouter);
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
