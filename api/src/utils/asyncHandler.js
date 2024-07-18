4c2e8ddb940cac14e43f8967d1101ce8717eb323
Asadullah-Imran <asadullahimran19@gmail.com>
2024-07-18 16:00:40 +0600

    crete utility for async handle, error  and response handle

---
+++ api/src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+// const asyncHandler=()=>{}
+// const asyncHandler=(func)=>{}
+// const asyncHandler=(func)=>{async()=>{}}
+// const asyncHandler=(func)=>async()=>{}
+
+//TRY-CATCH Part
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (err) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
+
+//PROMISES
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => {
+      next(err);
+    });
+  };
+};
+
+export { asyncHandler };
