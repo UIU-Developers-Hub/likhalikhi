7eff8fb342a186b2e5a1de4b675913bf45dcc53c
Asadullah-Imran <asadullahimran19@gmail.com>
2024-07-18 15:59:53 +0600

    set up Express

--- api/src/app.js
+++ api/src/app.js
@@ -1,5 +1,19 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
 
 const app = express();
+
+//app.use for middleware using
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //to get data from URL
+app.use(express.static("public")); // create a  folder named public and store img/png type data in server.
+app.use(cookieParser());
 
 export default app;
--- api/src/db/db.js
+++ api/src/db/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log(`\n connectionInstace : ${connectionInstance}`);
+    // console.log(`\n connectionInstace : ${connectionInstance}`);
     console.log(
       `\n MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`
     );
